5c42ab0075c4d40d3c5926cf4fde4cf1c4bc70cc
ADHITHYA BOLISHETTI <153520799+adhithya-bolishetti@users.noreply.github.com>
2026-05-23 15:11:48 +0530

    ✅ Add solution for: Abandoned Cart Analysis - SQL

---
+++ exercises/AbandonedCartAnalysisSQL.sql
@@ -0,0 +1,7 @@
+-- Write a SQL query below --
+SELECT c.email, ci.product_id
+FROM customers c
+JOIN carts ca ON c.id = ca.customer_id
+JOIN cart_items ci ON ca.id = ci.cart_id
+WHERE is_active = true
+ORDER BY email ASC;
